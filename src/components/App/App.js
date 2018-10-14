1bab001ecfe6d81ae8e34bb1c1d9be5302a4b768
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-10-14 20:14:41 +0200

    Add routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import { Header } from '../Header/Header';
+import { Footer } from '../Footer/Footer';
+import { MainPage } from '../MainPage/MainPage';
+import { Planner } from '../Planner/Planner';
+import { MealPage } from '../MealPage/MealPage';
+import { NotFound } from '../NotFound/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/' component={MainPage} />
+          <Route exact path='/planner' component={Planner} />
+          <Route exact path='/meals/:meal' component={MealPage} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
       </div>
     );
   }
