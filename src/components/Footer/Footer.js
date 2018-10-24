6605dff213d14451cf94f8e940c65ad7e9ed3e29
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-10-24 22:48:20 +0200

    Add content to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import './footer.css';
+
 export class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <div className="footer">
+        <p className="footer_signature">Made with &#10084; by Domi</p>
+      </div>
     );
   }
 };
